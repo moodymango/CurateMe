f600547e5686cec624fec91bc535afff630977b4
imz <duv.imma@gmail.com>
2022-06-24 19:02:41 -0400

    managed to successfully rendr individual card components on searchResults, with no images yet

--- client/components/artworkCard.jsx
+++ client/components/artworkCard.jsx
@@ -2,29 +2,25 @@ import React from 'react';
 //need to import card components in this file in order to render each artwork card
 const ArtworkCard = (props) => {
    //will inherent all necessary properties from search results component
+   //THIS FUNCTIONALITY WILL LIVE IN MY SEARCH CONTAINER FUNCTION
    //need to create an add to collection button that adds the artwork to user's specific collection on profile
    //will also need to add a delete button so user is able to delete it from their collection 
-    const artworkCards = [];
-        searchResults.forEach((artObj) => {
-            //here is where we will render the artworks, make sure to put a key on to know which to identify
-            //destructure each artObj  during each iteration, and pass those same properties from the object to the card component
-            const {
-                artist_title, 
-                artwork_type_title,
-                classification_titles,
-                date_display,
-                imageURL,
-                title,
-                medium_display
-            } = artObj;
-            console.log('artwork obj is =>', artObj)
-        })
+   const {
+    artist,
+    artwork_type ,
+    classifications,
+    date,
+    imageURL,
+    title,
+    medium,
+    artworkId 
+    } = props;
+
     return (
         <div className='search-results'>
             <h1>represents artwork results</h1>
-            {/* {artworkCards} */}
         </div>
     )
 }
 
-export default SearchResults;
+export default ArtworkCard;
--- client/components/searchResults.jsx
+++ client/components/searchResults.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 //need to import card components in this file in order to render each artwork card
+import ArtworkCard from './artworkCard.jsx';
 const SearchResults = (props) => {
     const {searchResults} = props;
     //going to render an array of smaller card components, each containing artwork info
@@ -15,14 +16,26 @@ const SearchResults = (props) => {
                 date_display,
                 imageURL,
                 title,
-                medium_display
+                medium_display,
+                artworkId
             } = artObj;
-            
+
+            artworkCards.push(
+            <ArtworkCard 
+                artist = {artist_title}
+                artwork_type = {artwork_type_title}
+                classifications = {classification_titles}
+                date = {date_display}
+                imageURL = {imageURL}
+                title = {title}
+                medium = {medium_display}
+                artworkId = {artworkId}
+            />)
         })
     return (
         <div className='search-results'>
             <h1>represents artwork results</h1>
-            {/* {artworkCards} */}
+            {artworkCards}
         </div>
     )
 }
