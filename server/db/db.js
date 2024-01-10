2f4bf9537890d4caf73589c4f9d4340d705a27f9
imz <duv.imma@gmail.com>
2024-01-10 16:34:29 -0500

    add pool connection to postgresql database in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,14 @@
-const postgres = require("postgres");
+const { Pool } = require("pg");
+
+const url = process.env.PG_URL;
+
+const pool = new Pool({
+  connectionString: url,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log("executed query", text);
+    return pool.query(text, params, callback);
+  },
+};
