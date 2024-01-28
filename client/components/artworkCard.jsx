37f2f167425cf256764202d97b07a75276e00fb1
imz <duv.imma@gmail.com>
2024-01-27 22:16:11 -0500

    feat: reconstruct image_url using image_id, image_url and small_image_url on artwork card component

--- client/components/artworkCard.jsx
+++ client/components/artworkCard.jsx
@@ -1,61 +1,63 @@
-import React, { useState, useEffect} from 'react';
-import axios from './api/axios';
+import React, { useState, useEffect } from "react";
+import axios from "./api/axios";
+const {
+  IMAGE_URL,
+  LARGE_IMAGE_URL,
+  SMALL_IMAGE_URL,
+} = require("../../server/models/config.js");
 //need to import card components in this file in order to render each artwork card
 const ArtworkCard = (props) => {
-   //will inherent all necessary properties from search results component
-   //THIS FUNCTIONALITY WILL LIVE IN MY SEARCH CONTAINER FUNCTION
-   //need to create an add to collection button that adds the artwork to user's specific collection on profile
-   //will also need to add a delete button so user is able to delete it from their collection 
-   const {
+  //will inherent all necessary properties from search results component
+  //THIS FUNCTIONALITY WILL LIVE IN MY SEARCH CONTAINER FUNCTION
+  //need to create an add to collection button that adds the artwork to user's specific collection on profile
+  //will also need to add a delete button so user is able to delete it from their collection
+  const {
     artist,
-    artwork_type ,
+    artwork_type,
     classifications,
     date,
-    imageURL,
+    image_id,
     title,
     medium,
     artworkId,
-    largeImageURL
-    } = props;
-    //create state for image api calls
-    //assuming I will be getting a url returned from my api call
-    const [images, setImages] = useState('')
-    const fetchImage = async () => {
-
-    }
-    //only need to display title, artist, date created, and medium,
-    //artworkID will be used to save the results from search, which will be done on searchContainer
-    return (
-        <div className='artwork-cards'>
-            {/* seperate div in order to show artwork image */}
-            {/* when user clicks on image, will be able to see a larger version */}
-            <div id = "art-image">
-                <img src ={imageURL} alt={title}/>
-            </div>
-            <div style = {{display: 'flex', flexDirection: 'column'}}>
-                <div id="artworkInfo" >
-                    <label htmlFor="title">Title: </label> 
-                    <span id="title">{title}</span>
-                </div>
-                <div id="artworkInfo" >
-                    <label htmlFor="artist">Artist: </label>
-                    <span id="artist">{artist}</span>
-                </div>
-                <div id="artworkInfo" >
-                    <label htmlFor="date">Date Created: </label>
-                    <span id="date">{date}</span>
-                </div>
-                <div id="artworkInfo" >
-                    <label htmlFor="medium">Medium: </label>
-                    <span id="medium">{medium}</span>
-                </div >
-                <div id = "artwork-card-buttons">
-                    {/* add onClick function that will save artwork to individual collection */}
-                    <button type = 'button'  >Add to Collection </button>
-                </div>
-            </div>
+  } = props;
+  //create state for image api calls
+  //assuming I will be getting a url returned from my api call
+  const [images, setImages] = useState("");
+  const fetchImage = async () => {};
+  //only need to display title, artist, date created, and medium,
+  //artworkID will be used to save the results from search, which will be done on searchContainer
+  return (
+    <div className="artwork-cards">
+      {/* seperate div in order to show artwork image */}
+      {/* when user clicks on image, will be able to see a larger version */}
+      <div id="art-image">
+        <img src={IMAGE_URL + image_id + SMALL_IMAGE_URL} alt={title} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div id="artworkInfo">
+          <label htmlFor="title">Title: </label>
+          <span id="title">{title}</span>
         </div>
-    )
-}
+        <div id="artworkInfo">
+          <label htmlFor="artist">Artist: </label>
+          <span id="artist">{artist}</span>
+        </div>
+        <div id="artworkInfo">
+          <label htmlFor="date">Date Created: </label>
+          <span id="date">{date}</span>
+        </div>
+        <div id="artworkInfo">
+          <label htmlFor="medium">Medium: </label>
+          <span id="medium">{medium}</span>
+        </div>
+        <div id="artwork-card-buttons">
+          {/* add onClick function that will save artwork to individual collection */}
+          <button type="button">Add to Collection </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ArtworkCard;
--- client/components/searchResults.jsx
+++ client/components/searchResults.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
+import React from "react";
 //need to import card components in this file in order to render each artwork card
-import ArtworkCard from './artworkCard.jsx';
+import ArtworkCard from "./artworkCard.jsx";
 const SearchResults = (props) => {
-    const {searchResults} = props;
-    //going to render an array of smaller card components, each containing artwork info
-        //need to iterate through search result array, rendering a new card component each time, making sure to pass down relevant info pertaining to artwork.
-    const artworkCards = [];
-        searchResults.forEach((artObj) => {
-            //here is where we will render the artworks, make sure to put a key on to know which to identify
-            //destructure each artObj  during each iteration, and pass those same properties from the object to the card component
-            const {
-                artist_title, 
-                artwork_type_title,
-                classification_titles,
-                date_display,
-                imageURL,
-                title,
-                medium_display,
-                artworkId,
-                largeImageURL
-            } = artObj;
+  const { searchResults } = props;
+  //going to render an array of smaller card components, each containing artwork info
+  //need to iterate through search result array, rendering a new card component each time, making sure to pass down relevant info pertaining to artwork.
+  const artworkCards = [];
+  searchResults.forEach((artObj) => {
+    //here is where we will render the artworks, make sure to put a key on to know which to identify
+    //destructure each artObj  during each iteration, and pass those same properties from the object to the card component
+    const {
+      artist_title,
+      artwork_type_title,
+      classification_titles,
+      date_display,
+      image_id,
+      title,
+      medium_display,
+      artworkId,
+    } = artObj;
 
-            artworkCards.push(
-            <ArtworkCard 
-                artist = {artist_title}
-                artwork_type = {artwork_type_title}
-                classifications = {classification_titles}
-                date = {date_display}
-                imageURL = {imageURL}
-                title = {title}
-                medium = {medium_display}
-                artworkId = {artworkId}
-                largeImageURL = {largeImageURL}
-            />)
-        })
-    return (
-        <div className='search-results'>
-            {artworkCards}
-        </div>
-    )
-}
+    artworkCards.push(
+      <ArtworkCard
+        artist={artist_title}
+        artwork_type={artwork_type_title}
+        classifications={classification_titles}
+        date={date_display}
+        image_id={image_id}
+        title={title}
+        medium={medium_display}
+        artworkId={artworkId}
+      />
+    );
+  });
+  return <div className="search-results">{artworkCards}</div>;
+};
 
 export default SearchResults;
