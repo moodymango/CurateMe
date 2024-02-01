c89c07062fdf4bc778e05fcc02b7afad683f20ba
imz <duv.imma@gmail.com>
2024-02-01 17:39:41 -0500

    remove extra home label on navBar

--- client/components/navBar.jsx
+++ client/components/navBar.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //must import link from react router in order to use
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 //will import a seach bar in order for the user to search
 //import login and signup components!
 //link to help make nav bar
@@ -11,34 +11,31 @@ class Navbar extends Component {
     super(props);
     this.state = {};
   }
-  render(){
-    return(
+  render() {
+    return (
       //using nav tag for accessibility reasons
-        <nav>
-            <ul id="nav-container">
-                <div id ="right-nav">
-                  {/* link replaces the <a> tag essentially! */}
-                 <li>
-                    <Link to="/" > Artsy Fartsy </Link>
-                  </li> 
-                  <li>
-                    <Link to="/" > Home </Link>
-                  </li> 
-                  <li>
-                    <Link to="/search" > Search for Artworks </Link>
-                  </li> 
-                </div>
-                <div id ="left-nav buttons ">
-                  <li>
-                    <Link to="/signup" > Sign Up </Link>
-                  </li> 
-                <li>
-                  <Link to="/login" > Login </Link>
-                </li> 
-                </div>
-            </ul>
-        </nav>
-    )
+      <nav>
+        <ul id="nav-container">
+          <div id="right-nav">
+            {/* link replaces the <a> tag essentially! */}
+            <li>
+              <Link to="/"> Artsy Fartsy </Link>
+            </li>
+            <li>
+              <Link to="/search"> Search for Artworks </Link>
+            </li>
+          </div>
+          <div id="left-nav buttons ">
+            <li>
+              <Link to="/signup"> Sign Up </Link>
+            </li>
+            <li>
+              <Link to="/login"> Login </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    );
   }
 }
 export default Navbar;
