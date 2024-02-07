bd8a77624622fcf132100775609e574625a62b3c
imz <duv.imma@gmail.com>
2024-02-06 21:35:39 -0500

    add useEffect hook to searchContainer to run fetchData function iff pageNumber changes

--- client/components/infiniteScroll.jsx
+++ client/components/infiniteScroll.jsx
@@ -2,22 +2,22 @@ import React, { useState, useEffect, useRef } from "react";
 //import child components like search results
 import SearchResults from "../components/searchResults.jsx";
 const InfiniteScroll = (props) => {
-  const { pageNum, handleSubmit, searchResults } = props;
+  const { changePage, pageNum, handleSubmit, searchResults } = props;
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  let localPage = pageNum;
   //checks if user has reached the bottom of the page and call fetchData if necessary
   // @source - https://stackoverflow.com/questions/3898130/check-if-a-user-has-scrolled-to-the-bottom-not-just-the-window-but-any-element
-  const handleScroll = () => {
-    //if how much has been scrolled down (scroll top) and height of the visible window (window.height)are equal to the total height of the document, we have reached the bottom of the page
+  // @source - https://blog.logrocket.com/3-ways-implement-infinite-scroll-react/ - from scratch implementation
+  const handleScroll = (e) => {
     if (
-      document.documentElement.scrollTop() + window.height() ==
-      document.height()
+      //if how much has been scrolled down (scroll top) and height of the visible window (window.height)are equal to the total height of the document, we have reached the bottom of the page
+      window.innerHeight + document.documentElement.scrollTop ==
+      document.documentElement.offsetHeight
     ) {
-      //want to update page num by 1, and then call fetchData function again
-      setPageNum((pageNum += 1));
+      //want to update page num by 1.
+      changePage();
     }
-    handleSubmit();
-    //want to call my fetch data function again with same params as last time
   };
   //add scroll event listener and remove whent the component is unmounted
   useEffect(() => {
--- client/containers/searchContainer.jsx
+++ client/containers/searchContainer.jsx
@@ -8,36 +8,45 @@ const SearchContainer = (props) => {
   //set initial state of search bar to empty string
   const [searchReq, setSearch] = useState("");
   const [categoryField, setCategoryField] = useState("");
-  const [pageNum, setPageNum] = useState(1);
+  const [pageNum, setPageNum] = useState(0);
   //set initial state of results (which should be an array of objs)
   const [searchResults, setResults] = useState([]);
   //set error messaging
   const [errMsg, setErrMsg] = useState("");
-  //make async function to fetch data
-  const fetchData = async () => {
-    try {
-      //within axios.post, need to define search url for backend
-      const apiResults = await axios.post(
-        "/search",
-        JSON.stringify({ searchReq, categoryField, pageNum }),
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
+
+  useEffect(() => {
+    //make async function to fetch data
+    async function fetchData() {
+      try {
+        //within axios.post, need to define search url for backend
+        const apiResults = await axios.post(
+          "/search",
+          JSON.stringify({ searchReq, categoryField, pageNum }),
+          {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+          }
+        );
+        //update state by calling setResults with callbacl form
+        //create new array containing the first parts I already had, and the results from the API
+        setResults((searchResults) => [...searchResults, ...apiResults.data]);
+      } catch (err) {
+        if (err.response) {
+          setErrMsg(`${err.response.data}`);
         }
-      );
-      setResults(apiResults.data);
-    } catch (err) {
-      if (err.response) {
-        setErrMsg(`${err.response.data}`);
       }
     }
-  };
-  //allows user to make calls via the api
+    fetchData();
+  }, [pageNum]);
+  //handle submission of the form
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("current category field is", categoryField);
-    fetchData();
-    //now need to pass down search results state to child component for this site
+    //page number will only update  when the handle submit function runs, which in turn will trigger our useEffect hook
+    setPageNum(1);
+  };
+  //function to change page
+  const changePage = () => {
+    setPageNum(pageNum + 1);
   };
   //function to capture value of radio buttons
   const handleChange = (e) => {
@@ -84,10 +93,11 @@ const SearchContainer = (props) => {
         </div>
       </form>
       <div id="search-results">
-        {searchResults.length ? (
+        {pageNum === 1 ? (
           //ideally would prefer to display infinite scroll here since search results will be nested within this function
           <InfiniteScroll
             handleSubmit={handleSubmit}
+            changePage={changePage}
             pageNum={pageNum}
             searchResults={searchResults}
           >
