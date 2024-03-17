6f723cc25a93afc7857827dd18a566086c8dc4fc
imz <duv.imma@gmail.com>
2024-03-17 04:23:05 -0400

    note: need to fix dimensions for masonry

--- client/components/searchResults.jsx
+++ client/components/searchResults.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 import ArtworkCard from "./artworkCard.jsx";
 import { forwardRef } from "react";
+//set spanning for all artwork cards
+//calculate height or spanning for any card based on the content wrapper height, row gap of grid, and size of implicit row tracks
+// function resizeResultItem(item) {
+//   var grid = document.getElementsByClassName("query-results"),
+//     rowGap = parseInt(
+//       window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
+//     ),
+//     rowHeight = parseInt(
+//       window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
+//     );
+//   //new height each artwork card (card height + grid row gap)
+//   let artworkCardHeight =
+//     item.querySelector(".artwork-content").getBoundingClientRect().height +
+//     rowGap;
+//   //net height of implicit row track (grid row gap + implicit grid row track)
+//   let netHeight = rowGap + rowHeight;
+
+//   //spanning for each artwork card depending on card height
+//   let rowSpan = Math.ceil(artworkCardHeight, netHeight);
+//   //assign each item a gridRowEnd property with calulated row span
+//   item.style.gridRowEnd = "span " + rowSpan;
+// }
+// function resizeResults() {
+//   const allCards = document.getElementsByClassName("artwork_card");
+//   allCards.forEach((card) => {
+//     imagesLoaded(card, (instance) => {
+//       let item = instance.elements[0];
+//       resizeResultItem(item);
+//     });
+//   });
+// }
+
 const SearchResults = forwardRef((props, ref) => {
   const { searchResults } = props;
   const artworkCards = [];
+  // let masonryEvents = ["load", "resize"];
+  // masonryEvents.forEach((event) => {
+  //   window.addEventListener(event, resizeAllMasonryItems);
+  // });
+  // resizeResults();
   searchResults.forEach((artObj, idx) => {
     const {
       artist_title,
