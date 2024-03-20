7d45adbcddfb7d14776b975fc1316ac949ed1136
imz <duv.imma@gmail.com>
2024-03-19 23:13:26 -0400

    note: need to refactor some of procedures into functions since I need to return certain results

--- server/db/procedures.sql
+++ server/db/procedures.sql
@@ -33,27 +33,30 @@ END;
 $$ LANGUAGE plpgsql;
 
 --finds user by username and returns the id and pass
-CREATE OR REPLACE PROCEDURE locate_user(
-    IN user VARCHAR(100), 
-    IN password_hash VARCHAR(100), 
-    OUT user_id INT)
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT id, first_name, password FROM users WHERE username=user;
-    --output success message
-    RAISE NOTICE 'Retrieved user by username "%".', username;
-END;
-$$ LANGUAGE plpgsql;
+--MAYBE HAVE TO TRANSFORM INTO A FUNCTION!!
+
+-- CREATE OR REPLACE PROCEDURE locate_user(
+--     userID IN VARCHAR(100), 
+--     password_hash IN VARCHAR(100),
+--     found_user INOUT NULL
+-- )
+-- AS $$
+-- BEGIN
+--     SELECT id, first_name, password INTO found_user FROM users WHERE username=userID;
+--     --output success message
+--     RAISE NOTICE 'Retrieved user by username "%".', username;
+-- END;
+-- $$ LANGUAGE plpgsql;
 
 --finds the user's favorite collection by id
 CREATE OR REPLACE PROCEDURE locate_favorites_id(
-    IN user INT, 
-    OUT fav_collection_id INT)
+    userID IN INT,
+    collection_id INOUT INT
+    )
 AS $$
 BEGIN
     --retrieve collection_id of user favorites collection
-    SELECT id FROM collections INTO user_collection_id WHERE user_id=user AND title='favorites';
+    SELECT id INTO collection_id FROM collections WHERE user_id=userID AND title='favorites';
 
     RAISE NOTICE 'Located user collection of favorites by id';
 END;
@@ -61,12 +64,14 @@ $$ LANGUAGE plpgsql;
 
 
 --finds the user's favorites by user_id and returns a table of artworks
-CREATE OR REPLACE PROCEDURE viewFavorites(IN user INT)
+-- WILL MOST LIKELY HAVE TO TRANSFORM INTO A FUNCTION
+CREATE OR REPLACE PROCEDURE viewFavorites(userID IN INT)
 AS $$
 DECLARE
     user_collection_id INT;
 BEGIN
-    CALL locate_favorites_id(user) INTO user_collection_id;
+    user_collection_id := CALL locate_favorites_id(userID);
+    -- CALL locate_favorites_id(userID) INTO user_collection_id;
 
     --find all artworks within the user collection based on user_collection_id
     RETURN QUERY 
@@ -77,15 +82,15 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+--upserts artwork into db and inserts artwork into favorite_artworks table
 CREATE OR REPLACE PROCEDURE favorite_artworks(
-    IN user INT, 
-    IN artworkID INT, 
-    IN artwork_title VARCHAR(150), 
-    IN artist VARCHAR(150), 
-    INT med VARCHAR(150), 
-    IN date_dis VARCHAR(150), 
-    IN imageID VARCHAR(150))
+    userID IN INT, 
+    artworkID IN INT, 
+    artwork_title IN VARCHAR(150), 
+    artist IN VARCHAR(150), 
+    med IN VARCHAR(150), 
+    date_dis IN VARCHAR(150), 
+    imageID IN VARCHAR(150))
 AS $$
 DECLARE
     art_id INT;
@@ -96,13 +101,14 @@ BEGIN
     AS(
          --upsert artwork into the db    
         INSERT INTO artworks(id, title, artist_title, medium, date_display, image_id) 
-         VALUES(artworkID, artwork_title, artist, med, date_dis, imageID) 
-         ON CONFLICT (id) DO NOTHING RETURNING id INTO art_id;
+        VALUES(artworkID, artwork_title, artist, med, date_dis, imageID) 
+        ON CONFLICT (id) DO NOTHING RETURNING id INTO art_id
     )
     SELECT * FROM artworkCTE UNION SELECT art_id FROM artworks WHERE image_id=imageID;
 
     --find user favorites collection by id
-    CALL locate_favorites_id(user) INTO fav_collection_id;
+    fav_collection_id := CALL locate_favorites_id(userID);
+    -- CALL locate_favorites_id(userID) INTO fav_collection_id;
 
     --insert artwork and collection id into favorite_artworks table
     INSERT INTO favorite_artworks(artwork_id, collection_id) VALUES(art_id, favorite_collection_id);
@@ -114,13 +120,14 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE PROCEDURE removeArtworkFromFavorites(
-    IN user INT, 
-    IN artworkID INT)
+    userID IN INT, 
+    artworkID IN INT)
 AS $$
 DECLARE
     user_collection_id INT;
 BEGIN
-    CALL locate_favorites_id(user) INTO user_collection_id;
+    user_collection_id := CALL locate_favorites_id(userID);
+    -- CALL locate_favorites_id(userID) INTO user_collection_id;
    --delete the artwork from the favorite_artworks table based on collection and artwork id
    DELETE FROM favorite_artworks WHERE collection_id=user_collection_id AND artwork_id=artworkID;
    RAISE NOTICE 'Artwork was removed from the user favorites collection', username, first_name;
